7e10046e4a9e080761891c9ea9e64cf036fa8c1f
Paulfinex <sentius96@gmail.com>
2019-04-18 01:36:48 +0100

    Tutorial move Phase

--- project/tutorial_scene.cpp
+++ project/tutorial_scene.cpp
@@ -12,6 +12,9 @@
 #include "prefabs_manager.h"
 #include "buttons_manager.h"
 #include "sound.h"
+#include "components/cmp_player_movement.h"
+#include "components/cmp_enemy_ai.h"
+#include "components/cmp_physics.h"
 extern SoundEffects s;
 
 using namespace std;
@@ -20,33 +23,70 @@ void TutorialScene::Load()
 {
 	_delay = 0.3f;
 	float offSet = 45.0f;
+	_infoText = Engine::GetActiveScene()->makeEntity();
+	auto b = _infoText->addComponent<ShapeComponent>();
+	b->setShape<RectangleShape>(Vector2f(165.0f, 30.0f));
+	b->getShape().setOrigin(b->getShape().getLocalBounds().width / 2 + 10.f, b->getShape().getLocalBounds().height / 2 + 10.f);
+	b->getShape().setFillColor(Color::Black);
+	auto t = _infoText->addComponent<TextComponent>("");
+	t->getText()->setOrigin(b->getShape().getLocalBounds().width / 2 + 10.f, b->getShape().getLocalBounds().height / 2 + 10.f);
+	float x = Engine::GetWindow().getView().getSize().x / 9;
+	float y = Engine::GetWindow().getView().getSize().y / 9;
+	t->getText()->setColor(Color::White);
 
-	_buttonBack = make_button("Main Menu");
-	_buttonBack->setPosition({  Engine::GetWindow().getView().getSize().x / 8,Engine::GetWindow().getView().getSize().y - Engine::GetWindow().getView().getSize().y/8 });
+	_infoText->setPosition(Vector2f{ x,y});
+
 
 	setLoaded(true);
 }
 
 void TutorialScene::Update(const double & dt)
 {
-	if (_delay > 0)
+	
+	switch (phase)
 	{
-		_delay -= dt;
-	}
-	auto mousePos = Engine::GetWindow().mapPixelToCoords(Mouse::getPosition(Engine::GetWindow()));
-	if (_delay <= 0)
-	{
-		_delay = 0.1f;
-		// Check buttons if selected
-		{
-			if (ButtonHandling(_buttonBack, mousePos))
-			{
-				Engine::ChangeScene(&mainMenu);
-			}
-		}
+	case 1:
+		phase1();
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	default:
+		break;
 	}
 
 	Scene::Update(dt);
+}
+
+
+
+
+void TutorialScene::phase1()
+{
+	if (tutorial_move)
+	{
+		_player.reset();
+		_player = tutorial_player();
+		_infoText->GetCompatibleComponent<TextComponent>()[0]->SetText("Welcome! This is your character, you can move using directional arrows, try it!");
+
+		tutorial_move = false;
+	}
+
+}
+
+void TutorialScene::phase2()
+{
+}
+
+void TutorialScene::phase3()
+{
+}
+
+void TutorialScene::phase4()
+{
 }
 
 
@@ -56,3 +96,34 @@ void TutorialScene::UnLoad()
 	Scene::UnLoad();
 }
 
+void TutorialScene::Render()
+{
+	Scene::Render();
+}
+
+
+
+std::shared_ptr<Entity> TutorialScene::tutorial_player()
+{
+
+	auto player = Engine::GetActiveScene()->makeEntity();
+	float x = Engine::GetWindow().getView().getSize().x / 2;
+	float y = Engine::GetWindow().getView().getSize().y / 2;
+	Vector2f pos = { x,y };
+	player->setPosition(pos);
+	player->addTag("player");
+	auto s = player->addComponent<SpriteComponent>();
+	auto tex = Resources::get<Texture>("tex.png");
+	s->setTexture(tex);
+	s->getSprite().setTextureRect(sf::IntRect(35, 0, 32, 32));
+	s->getSprite().setScale(1.5f, 1.5f);
+	s->getSprite().setOrigin(s->getSprite().getLocalBounds().width / 2, s->getSprite().getLocalBounds().height / 2);
+	player->addComponent<PlayerMovementComponent>();
+	auto p = player->addComponent<PhysicsComponent>(true, Vector2f(s->getSprite().getLocalBounds().width + 10.f, s->getSprite().getLocalBounds().height + 10.f));
+	p->getBody()->SetSleepingAllowed(false);
+	p->getBody()->SetFixedRotation(true);
+	p->getBody()->SetBullet(true);
+
+	Engine::GetActiveScene()->ents.list.push_back(player);
+	return player;
+}
--- project/tutorial_scene.h
+++ project/tutorial_scene.h
@@ -8,11 +8,25 @@ public:
 	~TutorialScene() override = default;
 	void Load() override;
 	void UnLoad() override;
+	void Render();
 	void Update(const double& dt) override;
 
 private:
 	std::shared_ptr<Entity> _buttonBack;
+	std::shared_ptr<Entity> _infoText;
+	std::shared_ptr<Entity> _player;
 	float _offSet = 45.0f;
 	double _delay;
 	double _iteratorDelay;
+	std::shared_ptr<Entity> tutorial_player();
+	bool tutorial_move = true;
+	bool tutorial_walls = true;
+	bool tutorial_treasure = true;
+	bool tutorial_ghost = true;
+	int phase = 1;
+	void phase1();
+	void phase2();
+	void phase3();
+	void phase4();
+
 };
